c66c9af53fee3ee2ef0e9a70b5a467eb60e3569d
Shefcoding <sheriff.b@live.nl>
2022-12-30 19:18:00 +0100

    update

--- lib.py
+++ lib.py
@@ -1,2 +1,22 @@
-def power(x, y):
+# Import what we need from flask
+from flask import Flask
+
+# Create a Flask app inside `app`
+app = Flask(__name__)
+
+# Assign a function to be called when the path `/` is requested
+@app.route('/')
+def power():
+    x=2
+    y=2
     return x**y
+
+@app.route('/cow')
+def cow():
+    return 'MOoooOo!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+
+
--- test_lib.py
+++ test_lib.py
@@ -1,5 +1,7 @@
 import lib
 
 def test_power():
-    assert lib.power(2,2) == 4
-    assert lib.power(2,3) == 8
+     assert lib.power() == 4
+     assert lib.cow()=='MOoooOo!'
+#    assert lib.power(2,2) == 4
+#    assert lib.power(2,3) == 8
