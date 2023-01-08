7da496adf5385b82b386a122ae6a6f9e9f1e93a1
Shefcoding <sheriff.b@live.nl>
2023-01-08 14:49:13 +0100

    Update test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -1,7 +1,7 @@
 import lib
 
 def test_power():
-     assert lib.power() == 4
+     assert lib.power() == '2 * 2 = 4'
      assert lib.cow()=='MOoooOoo!'
 #    assert lib.power(2,2) == 4
 #    assert lib.power(2,3) == 8
